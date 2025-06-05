7bf126f5e7efca91c3b8e8609844a10f4a70320f
mysleekdesigns <simonlacey20@gmail.com>
2025-06-05 13:29:18 -0400

    fixed scss to css issue

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
+const pluginSass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
+
 module.exports = function(eleventyConfig) {
+  // Add the SASS plugin
+  eleventyConfig.addPlugin(pluginSass);
+  
   // Copy assets
   eleventyConfig.addPassthroughCopy("src/images");
-  eleventyConfig.addPassthroughCopy("src/css");
+  // Remove the CSS passthrough copy since SASS plugin handles it
   
   return {
     dir: {
