ebe530e575c9700726dcf71b4de36bd0db097e4b
mysleekdesigns <simonlacey20@gmail.com>
2025-06-06 10:27:09 -0400

    setup dev branch

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,28 @@
 const pluginSass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Add the SASS plugin
   eleventyConfig.addPlugin(pluginSass);
-  
+
   // Copy assets
   eleventyConfig.addPassthroughCopy("src/images");
-  // Remove the CSS passthrough copy since SASS plugin handles it
-  
+
+  // Copy Bootstrap Icons font files
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/bootstrap-icons/font/fonts": "css/fonts",
+  });
+
+  // Copy Bootstrap JavaScript
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js":
+      "js/bootstrap.bundle.min.js",
+  });
+
   return {
     dir: {
       input: "src",
       output: "public",
-      includes: "_includes"
-    }
+      includes: "_includes",
+    },
   };
-}; 
+};
